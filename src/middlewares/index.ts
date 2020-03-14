29edf28acb211c7f7650eef214d1c2f5b6d5150f
Ahabal <lolreplay92@gmail.com>
2020-03-14 17:49:07 +0200

    [Added] - Comments

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -6,7 +6,7 @@ export function authenticateToken(req: any, res: Response, next: NextFunction) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token === "null") {
-        return res.status(401).send({message: 'Please login first'});
+        return res.status(401).send({message: 'Please sign in first'});
     }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: any, user: any) => {
         if (err) {
